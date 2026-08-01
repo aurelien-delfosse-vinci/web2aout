9f5471f8eb41a14cd044623873c3717eccac22db
aurelien.delfosse <aurelien.delfosse@student.vinci.be>
2026-08-01 16:18:48 +0200

    done ex1.1

---
+++ exercises/1.1/basic-ts-api-boilerplate/routes/films.ts
@@ -0,0 +1,46 @@
+import {Router} from "express";
+import { Film } from "../types";
+
+
+const defaultFilms: Film[] = [ 
+  {
+    id: 1,
+    title: "Inception",
+    director: "Christopher Nolan",
+    duration: 148,
+    budget: 160,
+    description:
+      "Un voleur spécialisé dans l'extraction de secrets à travers les rêves se voit confier une mission d'inception.",
+    imageUrl:
+      "https://image.tmdb.org/t/p/original/8IB2e4r4oVhHnANbnm7O3Tj6tF8.jpg",
+  },
+  {
+    id: 2,
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    duration: 169,
+    budget: 165,
+    description:
+      "Une équipe d'astronautes traverse un trou de ver afin de trouver une nouvelle planète habitable pour l'humanité.",
+    imageUrl:
+      "https://image.tmdb.org/t/p/original/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+  },
+  {
+    id: 3,
+    title: "The Matrix",
+    director: "Lana Wachowski & Lilly Wachowski",
+    duration: 136,
+    budget: 63,
+    description:
+      "Un pirate informatique découvre que la réalité dans laquelle il vit est une simulation créée par des machines.",
+    imageUrl:
+      "https://image.tmdb.org/t/p/original/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg",
+  },
+];
+
+const router = Router();
+
+router.get('/', (_req, res) => {
+    return res.json(defaultFilms);
+});
+export default router;
