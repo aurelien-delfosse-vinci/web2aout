ceae7040ab94196c6b6c47dfe6bace52894e1ec2
aurelien.delfosse <aurelien.delfosse@student.vinci.be>
2026-08-02 09:30:19 +0200

    lintified

--- exercises/1.2/basic-ts-api-boilerplate/app.ts
+++ exercises/1.2/basic-ts-api-boilerplate/app.ts
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/films", filmRouter)
+app.use("/films", filmRouter);
 
 export default app;
