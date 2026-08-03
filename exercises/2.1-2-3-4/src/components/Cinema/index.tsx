97ca2cc73707cf95fe401474fa1317276d7d771e
aurelien.delfosse <aurelien.delfosse@student.vinci.be>
2026-08-03 15:41:07 +0200

    new: ex2.3

---
+++ exercises/2.1-2-3-4/src/components/Cinema/index.tsx
@@ -0,0 +1,35 @@
+interface CinemaProps {
+  name:string;
+  movies: Movie[];
+};
+
+type Movie = {
+    title:string;
+    director:string;
+};
+
+const Cinema = (props:CinemaProps) => {
+  return (
+    <div>
+        <h2>{props.name}</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Film</th>
+                    <th>Director</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.movies.map((movie) => (
+                    <tr>
+                        <td>{movie.title}</td>
+                        <td>{movie.director}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+      </div>
+  );
+};
+
+export default Cinema;
