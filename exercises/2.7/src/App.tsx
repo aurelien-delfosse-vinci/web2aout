84a0b579f545cef01acc7afbbee9bd60385ab7db
aurelien.delfosse <aurelien.delfosse@student.vinci.be>
2026-08-04 15:47:15 +0200

    tuto before 2.7

---
+++ exercises/2.7/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+interface Movie {
+  title: string;
+  director:string;
+  duration: number;
+  imageUrl?:string;
+  description?:string;
+  budget?:number;
+}
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tutorials/front/medium-state/src/types.tsx
@@ -0,0 +1,7 @@
+type Pizza = {
+  id:number;
+  title:string;
+  content: string;
+}
+
+export default Pizza;
