41e5d558a9f70163cbf42d7bfa82e42ca785f285
aurelien.delfosse <aurelien.delfosse@student.vinci.be>
2026-08-06 15:07:00 +0200

    new: ex2.14b & 2.14b+

--- exercises/2.14b/src/App.tsx
+++ exercises/2.14b/src/App.tsx
@@ -1,61 +1,79 @@
-
-import { useEffect, useState } from 'react';
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 interface DogResponse {
-  message:string;
+  message: string;
   status: string;
 }
 
 const RandomDog = () => {
   const [image, setImage] = useState("");
+  const isHoveredRef = useRef(false);
+
+  const handleMouseEnter = () => {
+    isHoveredRef.current =true;
+   
+  }
+  const handleMouseLeave = () => {
+    isHoveredRef.current = false;
+  }
+
+  const fetchDog = async () => {
+    try {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+      if (!response.ok)
+        throw new Error(
+          `fetch error : ${response.status} : ${response.statusText}`,
+        );
+
+      const dog: DogResponse = await response.json();
+      setImage(dog.message);
+    } catch (err) {
+      console.log("RandomDog::error: ", err);
+    }
+  };
 
   useEffect(() => {
     fetchDog();
+
+    const interval = setInterval(() => {
+      if (!isHoveredRef.current) {
+        fetchDog();
+      }
+    }, 5000);
+
+    return () => clearInterval(interval);
   }, []);
 
-  const fetchDog = async () => {
-    try{
-      const response = await fetch("https://dog.ceo/api/breeds/image/random");
-      if(!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+  if (!image) return <p>Chargement...</p>;
 
-      const dog:DogResponse = await response.json();
-      setImage(dog.message);
-      
-    }catch(err){
-      console.log("RandomDog::error: ", err);
-    }
-    
-  }
-
-  if(!image) return <p>Chargement...</p>
-
-  return(
+  return (
     <div>
-      <img src={image} alt="Random dog" width={250} style={{borderRadius: "10px"}} />
+      <img
+        src={image}
+        alt="Random dog"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        width={250}
+        style={{ borderRadius: "10px" }}
+      />
     </div>
   );
 };
 
-
 function App() {
-  const[refresh, setRefresh] = useState(0);
-
-
   return (
     <>
       <div>
         <h1>Random Dogs 🐶</h1>
-        <div>
-          <RandomDog key={`${refresh}-1`}/>
-          <RandomDog key={`${refresh}-2`}/>
-          <RandomDog key={`${refresh}-3`}/>
+        <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
+          <RandomDog />
+          <RandomDog />
+          <RandomDog />
         </div>
-        <button onClick={() => setRefresh(refresh+1)}>Rafraichir</button>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
